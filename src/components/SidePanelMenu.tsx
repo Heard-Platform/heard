af73300ced6104f5fef4ec119584c90dd0b2d779
Alex Long <alexmasonlong@gmail.com>
2026-01-15 20:15:46 -0500

    Remove nick name from side panel

--- src/components/SidePanelMenu.tsx
+++ src/components/SidePanelMenu.tsx
@@ -155,9 +155,6 @@ export function SidePanelMenu({
 
         <div className="space-y-4 mt-6 overflow-y-auto flex-1 px-1">
           <div className="p-4 bg-green-50 border border-green-200 rounded-lg">
-            <p className="text-green-800">
-              <span className="font-medium">{user.nickname}</span>
-            </p>
             <p className="text-sm text-green-600 mt-1">
               Score: {user.score}
             </p>
