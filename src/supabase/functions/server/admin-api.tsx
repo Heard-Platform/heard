de28665c66bc1185dcd5346cc2a8735434d68cb6
Alex Long <alexmasonlong@gmail.com>
2026-02-16 22:08:24 -0500

    Newsletter #6!

--- src/supabase/functions/server/admin-api.tsx
+++ src/supabase/functions/server/admin-api.tsx
@@ -1,7 +1,7 @@
 import { getNewsletterEmail, getNewsletter2Email } from "./email-templates.tsx";
 import * as kv from "./kv_store.tsx";
 import { getActiveRooms } from "./debate-api.tsx";
-import { getAllRealDebates, getAllRealUsers, getAllStatements, getAllSubHeards, getByPrefixParsed, getDebate, getUser, saveDebate, phoneKvKeyFn, deletePhone, getCommunity, saveCommunity } from "./kv-utils.tsx";
+import { getAllRealDebates, getAllRealUsers, getAllStatements, getAllSubHeards, getByPrefixParsed, getDebate, getUser, saveDebate, phoneKvKeyFn, deletePhone, getCommunity, saveCommunity } from "././kv-utils.tsx";
 import { getVotesForUser, getUserActivityRecords } from "./kv-utils.tsx";
 import { DebateRoom, Rant, Statement } from "./types.tsx";
 import { saveUser } from "./kv-utils.tsx";
@@ -9,6 +9,7 @@ import { migrateAllUsersToSupabase } from "./migrate-users-to-supabase.tsx";
 import { getNewsletter3Email } from "./email-newsletter-3.ts";
 import { getNewsletter4Email } from "./email-newsletter-4.ts";
 import { getNewsletter5Email } from "./email-newsletter-5.ts";
+import { getNewsletter6Email } from "./email-newsletter-6.ts";
 import { sanitizeUser } from "./user-utils.ts";
 import { sendDebateCompletionCelebration } from "./cron-api.tsx";
 
@@ -659,6 +660,10 @@ app.post(
         const newsletter5 = getNewsletter5Email();
         subject = newsletter5.subject;
         getNewsletterHtml = () => newsletter5.html;
+      } else if (newsletterEdition === 6) {
+        const newsletter6 = getNewsletter6Email();
+        subject = newsletter6.subject;
+        getNewsletterHtml = () => newsletter6.html;
       } else {
         const getNewsletterHtmlFn = newsletterEdition === 2 ? getNewsletter2Email : newsletterEdition === 3 ? getNewsletter3Email : getNewsletterEmail;
         const newsletterSubjects = {
