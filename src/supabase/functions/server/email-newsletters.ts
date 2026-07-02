388988759856f9901ec2b768103b785b965b6731
Alex Long <alexmasonlong@gmail.com>
2026-07-02 14:13:03 -0400

    Undo

--- src/supabase/functions/server/email-newsletters.ts
+++ src/supabase/functions/server/email-newsletters.ts
@@ -317,7 +317,7 @@ const newsletters = [
   }),
   {}, // Blank because hardcoded funding email
   getParameterizedNewsletter({
-    subject: "DC is split on Waymo",
+    subject: "DC is split on Waymo - Heard Weekly #21",
     editionNumber: 21,
     section1: {
       title: "🚕 DC is split on Waymo",
