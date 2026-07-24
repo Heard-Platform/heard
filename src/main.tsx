35a21282c6e5f613da407d866f8411b1cdc52c1a
Greg <greghcarr@gmail.com>
2026-07-24 10:52:32 -0400

    Add i18n import to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import * as Sentry from "@sentry/react";
 import { createRoot } from "react-dom/client";
+import "./i18n";
 import App from "./App.tsx";
 import "./index.css";
 import "./semantic_classes.css";
